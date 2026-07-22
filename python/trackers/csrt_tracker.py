db19b2892393a0cb241aaf35210ee4d7ed43d634
ivyha010 <54672240+ivyha010@users.noreply.github.com>
2026-07-22 22:39:10 +0800

    Update csrt_tracker.py

--- python/trackers/csrt_tracker.py
+++ python/trackers/csrt_tracker.py
@@ -1,1 +1,19 @@
+import cv2
+
+class CSRTTracker:
+    def __init__(self):
+        try:
+            self.tracker = cv2.legacy.TrackerCSRT_create()
+        except AttributeError:
+            self.tracker = cv2.TrackerCSRT_create()
+        self.initialized = False
+
+    def init(self, frame, bbox):
+        self.tracker.init(frame, bbox)
+        self.initialized = True
+
+    def update(self, frame):
+        if not self.initialized:
+            raise RuntimeError("Tracker not initialized with init()")
+        return self.tracker.update(frame)
 
